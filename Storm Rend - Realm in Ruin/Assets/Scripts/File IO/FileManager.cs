41553540dd61a941ac37e34607bd743ba6ab6a64
Dillon Handyside <dj_handyside@hotmail.com>
2019-06-27 20:26:13 +1000

    Implemented file manager and started save data template class

---
+++ Storm Rend - Realm in Ruin/Assets/Scripts/File IO/FileManager.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+/// <summary>
+/// static file manager which allows the reading and writing of binary SaveData
+/// </summary>
+public static class FileManager
+{
+    /// <summary>
+    /// save function, uses binary formatting to serialize SaveData
+    /// </summary>
+    /// <param name="saveData">a custom class containing raw save data</param>
+    /// <param name="path">the path to save (e.g. Application.persistentDataPath + "data.sav")</param>
+    public static void Save(SaveData saveData, string path)
+    {
+        // create file stream for writing
+        FileStream file = new FileStream(path, FileMode.Create);
+
+        // save the data in binary format
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        binaryFormatter.Serialize(file, saveData);
+
+        // done with file stream
+        file.Close();
+    }
+
+    /// <summary>
+    /// load function, uses deserializes formatted binary save data into a SaveData
+    /// container
+    /// </summary>
+    /// <param name="path">the path to save (e.g. Application.persistentDataPath + "data.sav")</param>
+    /// <returns>a custom class containing raw save data</returns>
+    public static SaveData Load(string path)
+    {
+        // check if save data exists
+        if (!File.Exists(path))
+            return null; // no save data exists
+
+        // create file stream for reading
+        FileStream file = new FileStream(path, FileMode.Open);
+
+        // open the binary data
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        SaveData saveData = binaryFormatter.Deserialize(file) as SaveData;
+
+        // done with file stream
+        file.Close();
+        return saveData;
+    }
+}
