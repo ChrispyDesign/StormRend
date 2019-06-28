acf002853220cc5cd1c45207c1c2684a450c09c7
Dillon Handyside <dj_handyside@hotmail.com>
2019-06-28 12:25:48 +1000

    More blizzard juice. Copied hierarchy template scene to Dillon's scene

---
+++ Storm Rend - Realm in Ruin/Assets/Scripts/Input/ISelectable.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// selectable interface which forces selectable objects to implement OnSelect and OnDeselect
+/// </summary>
+public interface ISelectable
+{
+    void OnSelect();
+    void OnDeselect();
+}
