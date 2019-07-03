cdd7d94eab45db17df4dad740e13ede591fec674
Muhammad Ghalib <mg.gabe108@gmail.com>
2019-07-03 14:59:09 +1000

    Grid Highlight with Dijkstra Algo
    TODO:
    - make players move now
    
    BUGS:
    - Fixed some bugs relating OnSelect and OnDeselect but dillon will commit that

--- Storm Rend - Realm in Ruin/Assets/Scripts/Map/Node.cs
+++ Storm Rend - Realm in Ruin/Assets/Scripts/Map/Node.cs
@@ -16,7 +16,20 @@ public class Node : MonoBehaviour, IHoverable, ISelectable
     [SerializeField] private Node[] m_neighbours;
     [SerializeField] private Vector3 m_position;
     [SerializeField] private Vector2Int m_coordinate;
+
+    private bool m_selected = false;
+
     public NodeType m_nodeType;
+
+    public Node m_parent;
+    public int m_nGCost, m_nHCost;
+    public int m_nFCost
+    {
+        get
+        {
+            return m_nGCost + m_nHCost;
+        }
+    }
 
     public Node SetNodeVariables(Vector3 _pos, Vector2Int _coordinate, NodeType _nodeType)
     {
@@ -30,14 +43,14 @@ public class Node : MonoBehaviour, IHoverable, ISelectable
     public void SetUnitOnTop(Transform _unit) { m_unitOnTop = _unit; }
     public void SetNeighbours(Node[] _neighbours) { m_neighbours = _neighbours; }
 
-    List<Node> GetNeighbours()
+    public List<Node> GetNeighbours()
     {
         List<Node> neighbours = new List<Node>();
 
         foreach (Node node in m_neighbours)
         {
             if (node == null)
-                break;
+                continue;
 
             neighbours.Add(node);
         }
@@ -46,21 +59,39 @@ public class Node : MonoBehaviour, IHoverable, ISelectable
 
     public void OnHover()
     {
-        
+        transform.GetComponent<MeshRenderer>().material.color = Color.red;
     }
 
     public void OnUnhover()
     {
-
+        if (!m_selected)
+            transform.GetComponent<MeshRenderer>().material.color = Color.black;
     }
 
     public void OnSelect()
     {
+        if (!m_selected)
+        {
+            m_selected = true;
+            Dijkstra.Instance.FindValidMoves(this, 2);
+            List<Node> nodes = Dijkstra.Instance.m_validMoves;
 
+            foreach (Node n in nodes)
+            {
+                n.transform.GetComponent<MeshRenderer>().material.color = Color.red;
+                n.m_selected = true;
+            }
+        }
     }
 
     public void OnDeselect()
     {
-
+        m_selected = false;
+        List<Node> nodes = Dijkstra.Instance.m_validMoves;
+        foreach (Node n in nodes)
+        {
+            n.transform.GetComponent<MeshRenderer>().material.color = Color.black;
+            n.m_selected = false;
+        }
     }
 }
