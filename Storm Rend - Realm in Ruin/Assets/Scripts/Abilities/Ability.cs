9f960441a04af359ffcbf1812f3d0036ad032847
Dillon Handyside <dj_handyside@hotmail.com>
2019-07-03 16:55:34 +1000

    Created abstract base ability class

---
+++ Storm Rend - Realm in Ruin/Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct AbilityLevel
+{
+    [TextArea]
+    public string m_abilityEffect;
+    public int m_gloryRequirement;
+}
+
+public enum ExecutionOrder
+{
+    StartOfTurn,
+    EndOfTurn
+}
+
+public abstract class Ability : MonoBehaviour
+{
+    [SerializeField] private string m_abilityName;
+    [SerializeField] private AbilityLevel m_abilityEffectLevel1;
+    [SerializeField] private AbilityLevel m_abilityEffectLevel2;
+    [SerializeField] private AbilityLevel m_abilityEffectLevel3;
+    
+    public abstract void CastImmediately();
+    public abstract void CastDelayed(ExecutionOrder executionOrder, int turnDelay);
+}
