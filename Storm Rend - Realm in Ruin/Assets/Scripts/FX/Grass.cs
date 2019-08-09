3a4c745690d407eb74b8b1518ed286c58c02ec2e
Dale <d.giroud.grumley@gmail.com>
2019-08-09 17:01:34 +1000

    Did some animations stuff
    Dont judge me bro

--- Storm Rend - Realm in Ruin/Assets/Scripts/FX/Grass.cs
+++ Storm Rend - Realm in Ruin/Assets/Scripts/FX/Grass.cs
@@ -5,24 +5,20 @@ using UnityEngine;
 public class Grass : MonoBehaviour
 {
     [SerializeField] private GameObject grassTuft;
-    [SerializeField] private Vector2 speed;
-    [SerializeField] private Vector2 size;
+    [SerializeField] private List<Material> grass_mat;
+
+
 
     private void Awake()
-    {        
+    {
         int rand = Random.Range(0, 360);                                                                    //Sets a random Number
         transform.rotation = Quaternion.Euler(0, rand, 0);                                                  //Rotates The Tuft Randomly
 
-        
-        grassTuft.GetComponent<Renderer>().material.SetFloat("_Speed", Random.Range(speed.x, speed.y));     //Randomises the speed of the grass sway
+        rand = Random.Range(80, 120);                                                                       //Sets a random Number
+        grassTuft.transform.localScale = new Vector3((rand/100), (rand / 100), (rand / 100));               //Scales The Tuft Randomly
 
-        float rand1 = Random.Range(size.x, size.y);                                                              //Sets a random Number
-        grassTuft.transform.localScale = new Vector3(rand1, rand1, rand1);                                     //Randomises the speed of the grass sway
+        rand = Random.Range(0, grass_mat.Count);
 
-        rand = Random.Range(0, 2);                                                                          //Sets a random Number
-        if (rand >= 1)                                                                                      //Changers The Sway Direction
-           grassTuft.GetComponent<Renderer>().material.SetFloat("_Direction", 1);
-        if (rand < 1)
-            grassTuft.GetComponent<Renderer>().material.SetFloat("_Direction", -1);
+        grassTuft.GetComponent<Renderer>().material = grass_mat[rand];
     }
 }
