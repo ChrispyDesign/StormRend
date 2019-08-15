ff52b04a01f2d00d5b60e942c2614fc9a160ef11
Tony Le <letony@icloud.com>
2019-08-15 13:03:47 +1000

    Preliminary implementation of Attack Unit action delegate for enemies only

--- Assets/Scripts/AI/Delegates/AttackUnitAction.cs
+++ Assets/Scripts/AI/Delegates/AttackUnitAction.cs
@@ -1,4 +1,6 @@
-﻿using BhaVE.Core;
+﻿using System;
+using System.Collections.Generic;
+using BhaVE.Core;
 using BhaVE.Delegates;
 using BhaVE.Nodes;
 using BhaVE.Variables;
@@ -6,25 +8,45 @@ using UnityEngine;
 
 namespace StormRend.Bhaviours
 {
-	/// <summary>
-	/// Attacks the units in the list (hopefully only one left)
-	/// </summary>
-	// [CreateAssetMenu(menuName = "StormRend/Delegates/Actions/AttackUnit", fileName = "AttackUnit")]
-	public class AttackUnitAction : BhaveAction
+    /// <summary>
+    /// Attacks the units in the input list (hopefully only one left)
+    /// </summary>
+    [CreateAssetMenu(menuName = "StormRend/Delegates/Actions/AttackUnit", fileName = "AttackUnit")]
+    public class AttackUnitAction : BhaveAction
     {
-		[SerializeField] BhaveUnitList targets;
-		Unit unit;
+        [SerializeField] BhaveUnitList targets;
 
-		public override void Initiate(BhaveAgent agent)
-		{
-			unit = agent.GetComponent<Unit>();
-		}
+        Unit unit;
+
+        public override void Initiate(BhaveAgent agent)
+        {
+            unit = agent.GetComponent<Unit>();
+        }
 
         public override NodeState Execute(BhaveAgent agent)
         {
+			//Get abiltiies, get effects and then 
+            Ability passive = null;
+            Ability[] first = null, second = null;
+            unit.GetAbilities(ref passive, ref first, ref second);
+			List<Effect> effects = first[0].GetEffects();
 
-
-			return NodeState.Success;
+            foreach (var t in targets.value)
+            {
+				if (unit is EnemyUnit)
+				{
+					//Should be encapsulted
+					foreach(Effect effect in effects)
+					{
+						effect.PerformEffect(Grid.GetNodeFromCoords(t.m_coordinates), unit);
+					}
+				}
+				else if (unit is PlayerUnit)
+				{
+					throw new NotImplementedException();
+				}
+            }
+            return NodeState.Success;
         }
-	}
+    }
 }
