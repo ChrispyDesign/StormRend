0fe00d70be3f0368c020a4b5550328de51b27c6a
Muhammad Ghalib <mg.gabe108@gmail.com>
2019-08-22 17:28:15 +1000

    Bug Fixed: Dijkstra would return multiple instances of the same node

--- Assets/Scripts/Utility/Dijkstra.cs
+++ Assets/Scripts/Utility/Dijkstra.cs
@@ -74,7 +74,7 @@ namespace StormRend
 					}
 				}
 
-				if (currentNode.m_nGCost > 0 && currentNode.m_nGCost <= allowedTiles)
+				if (currentNode.m_nGCost > 0 && currentNode.m_nGCost <= allowedTiles && !m_validMoves.Contains(currentNode))
 					m_validMoves.Add(currentNode);
 
 			}
@@ -84,6 +84,9 @@ namespace StormRend
 				node.m_nHCost = 0;
 				node.m_nGCost = 0;
 			}
+
+			foreach (Tile tile in m_validMoves)
+				Debug.Log(tile.GetCoordinates());
 		}
 	}
 }
