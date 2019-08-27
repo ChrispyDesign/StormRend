8aa7e520760ed13741ddfc6c55403c43b43a24a5
Tony Le <letony@icloud.com>
2019-08-27 18:22:52 +1000

    Latest updates to BhaVE

---
+++ Assets/Scripts/FileIO/SaveData.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Save Data container class, contains relevant floats, ints and strings to be saved
+/// </summary>
+[System.Serializable]
+public class SaveData
+{
+    // the date/time of save
+    [SerializeField] private string m_dateTime;
+
+    #region getters
+    
+    public string GetDateTime() { return m_dateTime; }
+
+    #endregion
+
+    /// <summary>
+    /// constructor which initialises all of the raw save data
+    /// </summary>
+    /// <param name="dateTime"></param>
+    public SaveData(string dateTime)
+    {
+        m_dateTime = dateTime;
+    }
+}
