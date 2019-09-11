314d24428cf102ceeb1728318d2abd56bfd7e53f
Tony Le <letony@icloud.com>
2019-09-11 17:42:39 +1000

    Rename Tile.links to Tile.connections; Improve Connect and Disconnect

--- Assets/Scripts/CoreSystems/World/Tile.cs
+++ Assets/Scripts/CoreSystems/World/Tile.cs
@@ -4,25 +4,32 @@ using UnityEngine.EventSystems;
 
 namespace StormRend.Systems.Mapping
 {
-	/// <summary>
-	/// Base tile class. Holds a list of connections to neighbouring tiles. 
-	/// </summary>
-	public abstract class Tile : MonoBehaviour //, IPointerEnterHandler//, IPointerExitHandler, IPointerClickHandler
-	{
+    /// <summary>
+    /// Base tile class. Holds a list of connections to neighbouring tiles. 
+    /// </summary>
+    public abstract class Tile : MonoBehaviour //, IPointerEnterHandler//, IPointerExitHandler, IPointerClickHandler
+    {
 #if UNITY_EDITOR
-		public Color editorColor = Color.white;
+        public Color editorColor = Color.white;
 #endif
         public int ID;
-		
         public List<Link> links = new List<Link>();
+        public List<Tile> connections = new List<Tile>();
+        public float cost = 1;
         public float G = float.MaxValue;
         public float H = float.MaxValue;
         public float F = 0;
 
-		//Connect to a tile
-        public void Connect(Tile to, float cost = 1f) => links.Add(new Link(to, cost));
+        //Connect to a tile
+        public void Connect(Tile to, float cost = 1f)
+        {
+            to.cost = cost;
+            connections.Add(to);
+        }
+        // public void Connect(Tile to, float cost = 1f) => links.Add(new Link(to, cost));
 
-		//Disconnect from a tile
-		public void Disconnect(Tile from) => links.RemoveAll(x => x.target == from);
-	}
+        //Disconnect from a tile
+        public void Disconnect(Tile from) => connections.Remove(from);
+        // public void Disconnect(Tile from) => links.RemoveAll(x => x.target == from);
+    }
 }
