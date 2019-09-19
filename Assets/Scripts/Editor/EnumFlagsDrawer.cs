3a2de15660ec2b4594942700711fa5fded7caf91
Tony Le <letony@icloud.com>
2019-09-19 15:10:30 +1000

    Improve scriptable hub

--- Assets/Scripts/Editor/EnumFlagsDrawer.cs
+++ Assets/Scripts/Editor/EnumFlagsDrawer.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using StormRend.Utility.Attributes;
 
-namespace StormRend.Editor.PropDrawers
+namespace StormRend.Editors.PropDrawers
 {
     [CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
     public class EnumFlagsDrawer : PropertyDrawer
---
+++ Assets/Scripts/Editor/ScriptableHubEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using StormRend.Prototype;
+
+namespace StormRend.Editors
+{
+    [CustomEditor(typeof(ScriptableHub))]
+    public class ScriptableHubEditor : Editor
+    {
+        ScriptableHub sh;
+
+        void OnEnable()
+        {
+            sh = target as ScriptableHub;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Load All Scriptable Singletons"))
+            {
+                sh.LoadAllScriptableSingletons();
+            }
+            if (GUILayout.Button("Load All Scriptable Objects (Danger!)"))
+            {
+                sh.LoadAllScriptableObjects();
+            }
+            if (GUILayout.Button("Clear List"))
+            {
+                sh.scriptablesObjects.Clear();
+            }
+        }
+    }
+}
