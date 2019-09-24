3fa64e4d38006e473f7f5b3eb2f392c2e43f4d55
Dale <d.giroud.grumley@gmail.com>
2019-09-25 06:33:13 +1000

    Updated Canvas With new test layout

---
+++ Assets/Scripts/FX/Grass_2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass_2 : MonoBehaviour
+{
+    public GameObject boi;
+
+
+    public void Start()
+    {
+        Invoke("TurnOn", 0.1f);
+    
+    }
+
+    void TurnOn()
+    {
+        boi.SetActive(true);
+    }
+}
