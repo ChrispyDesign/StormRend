ceb0fc372a8e54aab404c73d8dd24ac138feab66
Tony Le <letony@icloud.com>
2019-10-16 12:41:29 +1100

    Mouse cursor changes in map editor depending on paint mode

--- Assets/Scripts/Editor/MapEditor.SceneEditor.cs
+++ Assets/Scripts/Editor/MapEditor.SceneEditor.cs
@@ -6,286 +6,297 @@ using UnityEngine;
 
 namespace StormRend.Editors
 {
-	//------------- Scene Editor --------------
-	public partial class MapEditor : SmartEditor
-	{
-		public enum EditMode { Painting, Erasing }
+    //------------- Scene Editor --------------
+    public partial class MapEditor : SmartEditor
+    {
+        public enum EditMode { Painting, Erasing }
 
-		const int kNumOfGridLines = 30;
-		public override bool RequiresConstantRepaint() => true;
+        const int kNumOfGridLines = 30;
+        public override bool RequiresConstantRepaint() => true;
 
-		Color oldHandleColor, oldGUIColor;
-		EditMode editMode;
+        Color oldHandleColor, oldGUIColor;
+        EditMode editMode;
 
-		int controlID;
-		bool isEditing;
+        int controlID;
+        bool isEditing;
 
 
-		#region Core
-		void OnSceneGUIBegin()
-		{
-			oldHandleColor = Handles.color;
-			oldGUIColor = GUI.color;
+        #region Core
+        void OnSceneGUIBegin()
+        {
+            oldHandleColor = Handles.color;
+            oldGUIColor = GUI.color;
 
-			//Events
-			e = Event.current;
+            //Events
+            e = Event.current;
 
-			//Hijack focus
-			controlID = GUIUtility.GetControlID(FocusType.Passive);
+            //Hijack focus
+            controlID = GUIUtility.GetControlID(FocusType.Passive);
 
-			SceneView.RepaintAll();
-		}
-		void OnSceneGUI()
-		{
-			OnSceneGUIBegin();
+            SceneView.RepaintAll();
+        }
+        void OnSceneGUI()
+        {
+            OnSceneGUIBegin();
 
-			DrawGrid(new Color(1f, 0.5f, 0));
-			DrawGridCursor();
+            DrawGrid(new Color(1f, 0.5f, 0));
+            SetMouseCursor();
+            DrawGridCursor();
 
-			if (showConnections) DrawConnections();
+            if (showConnections) DrawConnections();
 
-			if (!m || !m.selectedTilePrefab) return;
+            if (!m || !m.selectedTilePrefab) return;
 
-			DrawStamp(gridCursor);
+            DrawStamp(gridCursor);
 
-			HandleEvents();
+            HandleEvents();
 
-			OnSceneGUIEnd();
-		}
-		void OnSceneGUIEnd()
-		{
-			//Reset colors
-			Handles.color = oldHandleColor;
-			GUI.color = oldGUIColor;
-		}
-		#endregion
+            OnSceneGUIEnd();
+        }
 
-		#region Event Handling
-		void HandleEvents()
-		{
-			editMode = (e.control || e.command) ? EditMode.Erasing : EditMode.Painting;
-			switch (e.type)
-			{
-				case EventType.MouseDown:
-					HandleMouseDownEvents();
-					break;
-				case EventType.MouseUp:
-					isEditing = false;  //Stop editing
-					break;
-			}
-			ContinueEditing();
+        void SetMouseCursor()
+        {
+            switch (editMode)
+            {
+                case EditMode.Painting:
+                    EditorGUIUtility.AddCursorRect(new Rect(0, 0, 10000, 10000), MouseCursor.ArrowPlus);
+                    break;
+                case EditMode.Erasing:
+                    EditorGUIUtility.AddCursorRect(new Rect(0, 0, 10000, 10000), MouseCursor.ArrowMinus);
+                    break;
+            }
+        }
 
-			void HandleMouseDownEvents()
-			{
-				if (!e.alt)     //Let the user orbit
-				{
-					switch (e.button)
-					{
-						case 0: //Left mouse button
-							isEditing = true;
-							PerformEdit();
-							GUIUtility.hotControl = controlID;  //Prevent unselect
-							e.Use();
-							break;
-					}
-				}
-			}
+        void OnSceneGUIEnd()
+        {
+            //Reset colors
+            Handles.color = oldHandleColor;
+            GUI.color = oldGUIColor;
+        }
+        #endregion
 
-			void ContinueEditing()
-			{
-				if (isEditing)
-				{
-					PerformEdit();
-				}
-			}
-		}
-		#endregion  //Event Handling
+        #region Event Handling
+        void HandleEvents()
+        {
+            editMode = (e.control || e.command) ? EditMode.Erasing : EditMode.Painting;
+            switch (e.type)
+            {
+                case EventType.MouseDown:
+                    HandleMouseDownEvents();
+                    break;
+                case EventType.MouseUp:
+                    isEditing = false;  //Stop editing
+                    break;
+            }
+            ContinueEditing();
 
-		#region Draw
-		void DrawStamp(Vector3 center)
-		{
-			//Position
-			stamp.transform.position = center;
+            void HandleMouseDownEvents()
+            {
+                if (!e.alt)     //Let the user orbit
+                {
+                    switch (e.button)
+                    {
+                        case 0: //Left mouse button
+                            isEditing = true;
+                            PerformEdit();
+                            GUIUtility.hotControl = controlID;  //Prevent unselect
+                            e.Use();
+                            break;
+                    }
+                }
+            }
 
-			//Visibility
-			stamp.SetActive((editMode == EditMode.Painting) ? true : false);
-		}
-		void DrawGrid(Color color, float alpha = 0.9f)
-		{
-			var dottedLineSize = 2f;
+            void ContinueEditing()
+            {
+                if (isEditing)
+                {
+                    PerformEdit();
+                }
+            }
+        }
+        #endregion  //Event Handling
 
-			Handles.color = new Color(color.r, color.g, color.b, alpha);
-			var lineLength = kNumOfGridLines * m.tileSize * 0.5f;
-			//Z lines
-			for (int i = -kNumOfGridLines / 2; i <= kNumOfGridLines / 2; i++)
-			{
-				var start = new Vector3(m.tileSize * i, 0, -lineLength) + m.transform.position;
-				var end = new Vector3(m.tileSize * i, 0, lineLength) + m.transform.position;
-				Handles.DrawDottedLine(start, end, dottedLineSize);
-			}
-			//X lines
-			for (int i = -kNumOfGridLines / 2; i <= kNumOfGridLines / 2; i++)
-			{
-				var start = new Vector3(-lineLength, 0, m.tileSize * i) + m.transform.position;
-				var end = new Vector3(lineLength, 0, m.tileSize * i) + m.transform.position;
-				Handles.DrawDottedLine(start, end, dottedLineSize);
-			}
-		}
-		void DrawGridCursor(Color? color = null)
-		{
-			if (color == null) color = Color.white;
-			if (editMode == EditMode.Erasing) color = Color.red;
+        #region Draw
+        void DrawStamp(Vector3 center)
+        {
+            //Position
+            stamp.transform.position = center;
 
-			var ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
+            //Visibility
+            stamp.SetActive((editMode == EditMode.Painting) ? true : false);
+        }
+        void DrawGrid(Color color, float alpha = 0.9f)
+        {
+            var dottedLineSize = 2f;
 
-			if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, 1 << m.gameObject.layer))      //The hidden editor raycast plane will always be on the same layer as the map object
-			{
-				Handles.color = color.Value;
+            Handles.color = new Color(color.r, color.g, color.b, alpha);
+            var lineLength = kNumOfGridLines * m.tileSize * 0.5f;
+            //Z lines
+            for (int i = -kNumOfGridLines / 2; i <= kNumOfGridLines / 2; i++)
+            {
+                var start = new Vector3(m.tileSize * i, 0, -lineLength) + m.transform.position;
+                var end = new Vector3(m.tileSize * i, 0, lineLength) + m.transform.position;
+                Handles.DrawDottedLine(start, end, dottedLineSize);
+            }
+            //X lines
+            for (int i = -kNumOfGridLines / 2; i <= kNumOfGridLines / 2; i++)
+            {
+                var start = new Vector3(-lineLength, 0, m.tileSize * i) + m.transform.position;
+                var end = new Vector3(lineLength, 0, m.tileSize * i) + m.transform.position;
+                Handles.DrawDottedLine(start, end, dottedLineSize);
+            }
+        }
+        void DrawGridCursor(Color? color = null)
+        {
+            if (color == null) color = Color.white;
+            if (editMode == EditMode.Erasing) color = Color.red;
 
-				//Snap to the nearest grid square
-				Vector3 offsetHit = hit.point - m.transform.position;
-				Vector3 floor = new Vector3(Mathf.FloorToInt(offsetHit.x / m.tileSize), 0, Mathf.FloorToInt(offsetHit.z / m.tileSize));
-				Vector3 offset = m.transform.position;
-				Vector3 centre = new Vector3(m.tileSize * 0.5f, 0, m.tileSize * 0.5f);
+            var ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
 
-				gridCursor = floor * m.tileSize + offset + centre;
+            if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, 1 << m.gameObject.layer))      //The hidden editor raycast plane will always be on the same layer as the map object
+            {
+                Handles.color = color.Value;
 
-				//Draw grid cursor
-				Handles.RectangleHandleCap(0, gridCursor, Quaternion.AngleAxis(90, Vector3.right), m.tileSize * 0.499f, EventType.Repaint);
-				Handles.RectangleHandleCap(1, gridCursor, Quaternion.AngleAxis(90, Vector3.right), m.tileSize * 0.5f, EventType.Repaint);
-				Handles.RectangleHandleCap(2, gridCursor, Quaternion.AngleAxis(90, Vector3.right), m.tileSize * 0.501f, EventType.Repaint);
-			}
-		}
-		void DrawConnections(Color? color = null)
-		{
-			Handles.color = color == null ? Color.white : color.Value;
+                //Snap to the nearest grid square
+                Vector3 offsetHit = hit.point - m.transform.position;
+                Vector3 floor = new Vector3(Mathf.FloorToInt(offsetHit.x / m.tileSize), 0, Mathf.FloorToInt(offsetHit.z / m.tileSize));
+                Vector3 offset = m.transform.position;
+                Vector3 centre = new Vector3(m.tileSize * 0.5f, 0, m.tileSize * 0.5f);
 
-			foreach (var t in m?.tiles)
-			{
-				Vector3 start = t.transform.position;
+                gridCursor = floor * m.tileSize + offset + centre;
 
-				foreach (var c in t?.connections)
-				{
-					Vector3 end = c.transform.position;
+                //Draw grid cursor
+                Handles.RectangleHandleCap(0, gridCursor, Quaternion.AngleAxis(90, Vector3.right), m.tileSize * 0.499f, EventType.Repaint);
+                Handles.RectangleHandleCap(1, gridCursor, Quaternion.AngleAxis(90, Vector3.right), m.tileSize * 0.5f, EventType.Repaint);
+                Handles.RectangleHandleCap(2, gridCursor, Quaternion.AngleAxis(90, Vector3.right), m.tileSize * 0.501f, EventType.Repaint);
+            }
+        }
+        void DrawConnections(Color? color = null)
+        {
+            Handles.color = color == null ? Color.white : color.Value;
 
-					Handles.DrawLine(start, end);
+            foreach (var t in m?.tiles)
+            {
+                Vector3 start = t.transform.position;
 
-					// Handles.DrawBezier(start, end, start + Vector3.up, end + Vector3.up, Color.red, null, 2);
-					// Handles.DrawSphere(0, start, Quaternion.identity, m.tileSize / 20f);
-					// Handles.color = new Color(0.6f, 1, 0.1f);
-				}
-			}
-		}
-		#endregion
+                foreach (var c in t?.connections)
+                {
+                    Vector3 end = c.transform.position;
 
-		#region Edit
-		void CreateStamp()
-		{
-			//Kill all children
-			while (stamp.transform.childCount > 0)
-				DestroyImmediate(stamp.transform.GetChild(0).gameObject);
+                    Handles.DrawLine(start, end);
 
-			//Recreate at cursor position
-			if (!m.selectedTilePrefab) return;	//null check
-			var go = Instantiate(m.selectedTilePrefab, gridCursor, Quaternion.identity);
-			go.transform.SetParent(stamp.transform);
-		}
-		void PerformEdit()
-		{
-			if (editMode == EditMode.Painting)
-				PerformStamp();
-			else
-				PerformErase();
-		}
-		void PerformStamp()
-		{
-			EditorGUIUtility.AddCursorRect(new Rect(0, 0, 10000, 10000), MouseCursor.ArrowPlus);
+                    // Handles.DrawBezier(start, end, start + Vector3.up, end + Vector3.up, Color.red, null, 2);
+                    // Handles.DrawSphere(0, start, Quaternion.identity, m.tileSize / 20f);
+                    // Handles.color = new Color(0.6f, 1, 0.1f);
+                }
+            }
+        }
+        #endregion
 
-			//Make sure there are no tiles in the current position
-			if (IsOverTile(gridCursor, m.tileSize * 0.95f, out GameObject tileHit))
-			{
-				Debug.LogWarning("Cannot paint on existing tile!");
-				return;
-			}
+        #region Edit
+        void CreateStamp()
+        {
+            //Kill all children
+            while (stamp.transform.childCount > 0)
+                DestroyImmediate(stamp.transform.GetChild(0).gameObject);
 
-			//Instantiate a new tile prefab
-			var rotation = randomizePaintDirection ? Quaternion.AngleAxis(90 * UnityEngine.Random.Range(0, 4), Vector3.up) : Quaternion.identity;
-			var newTile = Instantiate(m.selectedTilePrefab, gridCursor, rotation);	//Instantiate
-			newTile.transform.SetParent(m.transform);		//Parent
-			newTile.gameObject.layer = m.gameObject.layer;	//Layer
-			newTile.owner = m;	//Owner
+            //Recreate at cursor position
+            if (!m.selectedTilePrefab) return;  //null check
+            var go = Instantiate(m.selectedTilePrefab, gridCursor, Quaternion.identity);
+            go.transform.SetParent(stamp.transform);
+        }
+        void PerformEdit()
+        {
+            if (editMode == EditMode.Painting)
+                PerformStamp();
+            else
+                PerformErase();
+        }
+        void PerformStamp()
+        {
+            //Make sure there are no tiles in the current position
+            if (IsOverTile(gridCursor, m.tileSize * 0.95f, out GameObject tileHit))
+            {
+                Debug.LogWarning("Cannot paint on existing tile!");
+                return;
+            }
 
-			Undo.RegisterCreatedObjectUndo(newTile, "Paint Tile " + m.selectedTilePrefab.name);
+            //Instantiate a new tile prefab
+            var rotation = randomizePaintDirection ? Quaternion.AngleAxis(90 * UnityEngine.Random.Range(0, 4), Vector3.up) : Quaternion.identity;
+            var newTile = Instantiate(m.selectedTilePrefab, gridCursor, rotation);  //Instantiate
+            newTile.transform.SetParent(m.transform);       //Parent
+            newTile.gameObject.layer = m.gameObject.layer;  //Layer
+            newTile.owner = m;  //Owner
 
-			//Add to map's list of tiles
-			m.tiles.Add(newTile.GetComponent<Tile>());
-		}
-		void PerformErase()
-		{
-			EditorGUIUtility.AddCursorRect(new Rect(0, 0, 10000, 10000), MouseCursor.ArrowMinus);
+            Undo.RegisterCreatedObjectUndo(newTile, "Paint Tile " + m.selectedTilePrefab.name);
 
-			if (IsOverTile(gridCursor, m.tileSize * 0.95f, out GameObject tileToErase))
-			{
-				//TODO Temporary-better-than-nothing-solution
-				//Just clear all the connections to prevent null reference exceptions
-				m.ClearAllTileConnections();
+            //Add to map's list of tiles
+            m.tiles.Add(newTile.GetComponent<Tile>());
+        }
+        void PerformErase()
+        {
+            if (IsOverTile(gridCursor, m.tileSize * 0.95f, out GameObject tileToErase))
+            {
+                //TODO Temporary-better-than-nothing-solution
+                //Just clear all the connections to prevent null reference exceptions
+                m.ClearAllTileConnections();
 
-				//Erase the found tile
-				m.tiles.Remove(tileToErase.GetComponent<Tile>());
+                //Erase the found tile
+                m.tiles.Remove(tileToErase.GetComponent<Tile>());
 
-				Undo.DestroyObjectImmediate(tileToErase);
-			}
-		}
-		#endregion
+                Undo.DestroyObjectImmediate(tileToErase);
+            }
+        }
+        #endregion
 
-		#region Assists
-		void OnUndoRedo()
-		{
-			//Clean up mess left behind from Undo system
-			//Add tiles that aren't in the list
-			for (int i = 0; i < m.transform.childCount; i++)
-			{
-				var childTile = m.transform.GetChild(i).GetComponent<Tile>();
-				if (!m.tiles.Contains(childTile))
-					m.tiles.Add(childTile);
-			}
+        #region Assists
+        void OnUndoRedo()
+        {
+            //Clean up mess left behind from Undo system
+            //Add tiles that aren't in the list
+            for (int i = 0; i < m.transform.childCount; i++)
+            {
+                var childTile = m.transform.GetChild(i).GetComponent<Tile>();
+                if (!m.tiles.Contains(childTile))
+                    m.tiles.Add(childTile);
+            }
 
-			//Remove all tiles that are null
-			m.tiles.RemoveAll(x => !x);
-		}
-		/// <summary>
-		/// Returns true if a position is over a tile. Outs the tile it is over
-		/// </summary>
-		bool IsOverTile(Vector3 checkPos, float checkBoundsSize, out GameObject intersectedTile)
-		{
-			//BRUTE FORCE; Probably not very efficient
-			if (boundsType == BoundsType.RendererBounds)
-			{
-				var cursorBoundsSize = new Vector3(checkBoundsSize, float.MaxValue, checkBoundsSize);
-				var cursorBounds = new Bounds(gridCursor, cursorBoundsSize);
+            //Remove all tiles that are null
+            m.tiles.RemoveAll(x => !x);
+        }
+        /// <summary>
+        /// Returns true if a position is over a tile. Outs the tile it is over
+        /// </summary>
+        bool IsOverTile(Vector3 checkPos, float checkBoundsSize, out GameObject intersectedTile)
+        {
+            //BRUTE FORCE; Probably not very efficient
+            if (boundsType == BoundsType.RendererBounds)
+            {
+                var cursorBoundsSize = new Vector3(checkBoundsSize, float.MaxValue, checkBoundsSize);
+                var cursorBounds = new Bounds(gridCursor, cursorBoundsSize);
 
-				for (int i = 0; i < m.transform.childCount; ++i)
-				{
-					var child = m.transform.GetChild(i);
+                for (int i = 0; i < m.transform.childCount; ++i)
+                {
+                    var child = m.transform.GetChild(i);
 
-					//Check child is overlapping
-					var bounds = child.GetComponentInChildren<Renderer>().bounds;
+                    //Check child is overlapping
+                    var bounds = child.GetComponentInChildren<Renderer>().bounds;
 
-					//Tile found
-					if (cursorBounds.Intersects(bounds))
-					{
-						intersectedTile = child.gameObject;
-						return true;
-					}
-				}
-			}
-			else if (boundsType == BoundsType.ColliderBounds)
-			{
+                    //Tile found
+                    if (cursorBounds.Intersects(bounds))
+                    {
+                        intersectedTile = child.gameObject;
+                        return true;
+                    }
+                }
+            }
+            else if (boundsType == BoundsType.ColliderBounds)
+            {
 
-			}
-			intersectedTile = null;
-			return false;
-		}
-		#endregion
-	}
+            }
+            intersectedTile = null;
+            return false;
+        }
+        #endregion
+    }
 }
