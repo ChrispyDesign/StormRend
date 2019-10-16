0dce9d8201c1a2475020749c4449879ec4089b67
Tony Le <letony@icloud.com>
2019-10-16 12:40:17 +1100

    Improve sequence trigger zone

--- Assets/Scripts/Sequencing/TriggerZone.cs
+++ Assets/Scripts/Sequencing/TriggerZone.cs
@@ -1,9 +1,15 @@
 using System.Collections.Generic;
 using UnityEngine;
+using StormRend.Utility;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace StormRend.Sequencing
 {
 	/// <summary>
+	/// NOTE!: Maybe cinemachine already has somethinglike this	
 	/// Attach to objects that will be used to detect objects 
 	/// and then trigger it's sequence controller
 	/// </summary>
@@ -13,8 +19,8 @@ namespace StormRend.Sequencing
 	{
 		[SerializeField] SequenceController sequenceController;
 
-		[Header("If empty will detect ANY object with collider")]
-		[Tooltip("Detect only these objects in trigger zone")]
+		[Tooltip("If empty will detect ANY object with collider, otherwise detect only these objects")]
+		[SerializeField] LayerMask layerMask = ~0;
 		[SerializeField] List<TriggerObject> triggerObjects = new List<TriggerObject>();
 
 		Collider col;
@@ -37,28 +43,38 @@ namespace StormRend.Sequencing
 			rb.isKinematic = true;
 		}
 
-		void OnTriggerEnter(Collider collider)
+		void OnTriggerEnter(Collider col)
 		{
-			//Detect ANY collider
-			if (triggerObjects.Count <= 0)
+			//Detect ANY collider as long as it's in the selected layermask
+			if (triggerObjects.Count <= 0 && layerMask.Contains(col.gameObject.layer))
 			{
 				sequenceController.Play();
 			}
 			//Detect only Trigger Objects
 			else
 			{
-				var triggerObjectHit = collider.GetComponent<TriggerObject>();
-				if (triggerObjects.Contains(triggerObjectHit))
+				var triggerObjectHit = col.GetComponent<TriggerObject>();
+				if (triggerObjects.Contains(triggerObjectHit) && layerMask.Contains(triggerObjectHit.gameObject.layer))
 					sequenceController.Play();	
 			}
 		}
 
 		void OnDrawGizmos()
 		{
+			//Draw Red Wireframe Box
 			var oldColor = Gizmos.color;
 			Gizmos.color = Color.red;
 			if (col) Gizmos.DrawWireCube(col.bounds.center, col.bounds.size);
 			Gizmos.color = oldColor;
+#if UNITY_EDITOR
+			//Draw Trigger Zone Text
+			GUIStyle centeredLabel = new GUIStyle();
+			centeredLabel.alignment = TextAnchor.MiddleCenter;
+			centeredLabel.fontSize = 15;
+			centeredLabel.normal.textColor = Color.white;
+			using (new Handles.DrawingScope())
+				Handles.Label(transform.position, "TriggerZone: " + name, centeredLabel);
+#endif
 		}
 	}
 }
