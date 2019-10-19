90535c41c9a0c543669ad69dbfe6ea659a8e28f0
Tony Le <letony@icloud.com>
2019-10-19 16:17:37 +1100

    Improve MasterCamera

--- Assets/Scripts/Camera/MasterCamera.cs
+++ Assets/Scripts/Camera/MasterCamera.cs
@@ -1,25 +1,38 @@
+using pokoro.Patterns.Generic;
 using UnityEngine;
 
 namespace StormRend.CameraSystem
 {
 	/// <summary>
-	/// This is used as a tag to find locate the main working camera
+	/// This is used as a tag to find locate the main working camera.
+	/// If you use cinemachine you'd only need one master camera
 	/// </summary>
 	[RequireComponent(typeof(Camera))]
-	public class MasterCamera : MonoBehaviour
+	public class MasterCamera : Singleton<MasterCamera>
 	{
 		public int priority = 10;
-		new Camera camera { get; set; } = null;
-		void Awake()
+		public Camera linkedCamera 
 		{
-			camera = GetComponentInChildren<Camera>();	//Find below
-			if (camera) return;
-			camera = GetComponentInParent<Camera>();	//Find above
-			if (camera) return;
-			Debug.Assert(camera, "No camera found on this object!");
+			get 
+			{
+				if (!_linkedCamera) Reset();
+				return _linkedCamera;
+			}
+			private set => linkedCamera = value;
+		}
+		Camera _linkedCamera;
+
+		void Reset()
+		{
+			_linkedCamera = GetComponent<Camera>();
+			if (_linkedCamera) return;
+			_linkedCamera = GetComponentInChildren<Camera>();	//Find below
+			if (_linkedCamera) return;
+			_linkedCamera = GetComponentInParent<Camera>();		//Find above
+			Debug.Assert(_linkedCamera, "No camera found on this object!");
 		}
 
 		//Returns the attached camera
-		public static implicit operator Camera(MasterCamera rhs) => rhs.camera;
+		public static implicit operator Camera(MasterCamera rhs) => rhs.linkedCamera;
 	}
 }
