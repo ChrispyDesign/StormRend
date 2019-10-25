2ee385039cd0e4a02409499ec1586984c18e2a91
Muhammad Ghalib <mg.gabe108@gmail.com>
2019-10-25 16:52:49 +1100

    Snow Demo

---
+++ Assets/Shader/Snow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snow : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+		GetComponent<Renderer>().material.SetFloat("_Displacement", Random.Range(0.0f, 2.0f));
+    }
+}
