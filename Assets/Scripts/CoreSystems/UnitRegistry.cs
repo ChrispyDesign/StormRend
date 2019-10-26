6910178cbe901dbfd5b81da618b79a9121a17e17
Tony Le <letony@icloud.com>
2019-10-27 00:27:27 +1100

    Add status effect runner methods

--- Assets/Scripts/CoreSystems/UnitRegistry.cs
+++ Assets/Scripts/CoreSystems/UnitRegistry.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using pokoro.Patterns.Generic;
 using StormRend.MapSystems.Tiles;
+using StormRend.States;
+using StormRend.Systems.StateMachines;
 using StormRend.Utility.Attributes;
 using UnityEngine;
 
@@ -22,35 +24,84 @@ namespace StormRend.Units
 
 		void Start()
 		{
+			_deadUnits.Clear();
 			FindAllUnits();
-
-			void FindAllUnits()
-			{
-				_aliveUnits = FindObjectsOfType<Unit>().ToList();
-			}
+		}
+		
+		//Finds all units and sorts them based on whether they're dead or not
+		public void FindAllUnits()
+		{
+			_aliveUnits = FindObjectsOfType<Unit>().ToList();
+			foreach (var a in _aliveUnits)
+				if (a.isDead)
+					RegisterDeath(a);
 		}
 
 	#region Core
+		//Adds a new unit to the alive registry
 		public void RegisterUnit(Unit u) => _aliveUnits.Add(u);
 		public T[] GetUnitsByType<T>() where T : Unit => _aliveUnits.Where(x => (x is T)).ToArray() as T[];
+		//Register's the death of a unit and moves it from the alive to dead list
 		public void RegisterDeath(Unit deadUnit)
 		{
 			if (_aliveUnits.Remove(deadUnit))
-			{
 				_deadUnits.Add(deadUnit);
-			}
 			else
-			{
 				Debug.LogWarningFormat("{0} was not in list of alive units!", deadUnit);
+		}
+	#endregion
+
+	#region Status Effect Runners
+		public void RunStatusEffectOnTurnEnter(State state)
+		{
+			switch (state)
+			{
+				case AllyTurnState allyTurnState:
+					var allies = GetUnitsByType<AllyUnit>();
+					foreach (var a in allies)
+						a.BeginTurn();
+					break;
+				case EnemyTurnState enemyTurnState:
+					var enemies = GetUnitsByType<EnemyUnit>();
+					foreach (var e in enemies)
+						e.BeginTurn();
+					break;
+			}
+		}
+		public void RunStatusEffectsOnTurnExit(State state)
+		{
+			switch (state)
+			{
+				case AllyTurnState allyTurnState:
+					var allies = GetUnitsByType<AllyUnit>();
+					foreach (var a in allies)
+						a.EndTurn();
+					break;
+				case EnemyTurnState enemyTurnState:
+					var enemies = GetUnitsByType<EnemyUnit>();
+					foreach (var e in enemies)
+						e.EndTurn();
+					break;
 			}
 		}
 	#endregion
 
-	#region On Tile Utility Functions
+	#region OnTile Utility Functions
 		/// <summary>
-		/// Check if there's a unit on the tile. If so return true and output the unit
+		/// Returns true if ANY unit is on tje tile. Slightly more efficient than TryGetUnitTypeOnTile<>()
 		/// </summary>
-		public static bool TryGetUnitOnTile(Tile tile, out Unit unit)
+		public static bool IsAnyUnitOnTile(Tile tile)
+		{
+			foreach (var u in current.aliveUnits)
+				if (u.currentTile == tile)
+					return true;
+			return false;
+		}
+
+		/// <summary>
+		/// Returns true and outputs unit if ANY unit is on a the tile
+		/// </summary>
+		public static bool TryGetAnyUnitOnTile(Tile tile, out Unit unit)
 		{
 			foreach (var u in current.aliveUnits)
 				if (u.currentTile == tile)
@@ -65,7 +116,7 @@ namespace StormRend.Units
 		/// <summary>
 		/// Check if the unit of specified type is on the tile
 		/// </summary>
-		public static bool IsUnitTypeOnTile<T>(Tile tile, out T unit) where T : Unit
+		public static bool TryGetUnitTypeOnTile<T>(Tile tile, out T unit) where T : Unit
 		{
 			var filteredUnits = current.aliveUnits.Where(x => x is T);
 			foreach (var u in filteredUnits)
@@ -81,7 +132,7 @@ namespace StormRend.Units
 		}
 
 		/// <summary>
-		/// Returns true if any unit is standing on tile and also is or is derived from one of the typesToCheck
+		/// Returns true if any unit or any unit derived from one of the types passed in is standing on the tile
 		/// </summary>
 		/// <returns></returns>
 		public static bool AreUnitTypesOnTile(Tile tile, params Type[] typesToCheck)
