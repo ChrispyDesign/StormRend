2b7f945bb335159bb25c082ad921c16263d95617
Tony Le <letony@icloud.com>
2019-10-31 19:16:26 +1100

    Add State Machine Behaviour HandleDeathAnimation

--- Assets/Scripts/AnimatorBehaviours/HandleDeathAnimation.cs
+++ Assets/Scripts/AnimatorBehaviours/HandleDeathAnimation.cs
@@ -6,23 +6,14 @@ namespace StormRend.AnimatorBehaviours
     /// <summary>
     /// Checks if the unit has died (after this HitReact animation) and if so proceed to trigger the death animation
     /// </summary>
-    public class CheckIsDead : StateMachineBehaviour
+    public class HandleDeathAnimation : StateMachineBehaviour
     {
         AnimateUnit unit;   //The unit attached to this animator
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             unit = animator.GetComponentInParent<AnimateUnit>();
-
-            //Check if the unit is dead after being hit
-            if (unit.isDead)
-            {
-                //If so then run death animation
-                animator.SetTrigger("isDead");
-            }
-            else
-            {
-                animator.SetInteger("AttackAnim", 0);
-            }
+            //If unit has died then go int death animation
+            if (unit.isDead) animator.SetBool("isDead", true);
         }
     }
 }
