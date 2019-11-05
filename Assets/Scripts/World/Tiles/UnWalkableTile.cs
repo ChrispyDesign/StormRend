24615fa63453814c412c2f223fe3c18ae703a970
Tony Le <letony@icloud.com>
2019-11-06 03:33:21 +1100

    Fix unwalkable tile gizmos; Snow materials update

--- Assets/Scripts/World/Tiles/UnWalkableTile.cs
+++ Assets/Scripts/World/Tiles/UnWalkableTile.cs
@@ -5,20 +5,21 @@ namespace StormRend.MapSystems.Tiles
 {
 	public class UnWalkableTile : Tile
 	{
-		const float lily = 0.01f;
+		const float lily = 0.01f;	//A little y
 		void OnDrawGizmos()
 		{
 			//Draw an X over the tile in editor
+			var pos = transform.position;
 			var oldColor = Gizmos.color;
 			Gizmos.color = Color.black * 0.9f;
 			var min = rend.bounds.min;
 			var max = rend.bounds.max;
 			var flatbox = new Vector3(rend.bounds.size.x, lily, rend.bounds.size.z);
 			Gizmos.DrawWireCube(transform.position, flatbox);
-			var topLeft = new Vector3(min.x, lily, max.z);
-			var bottomRight = new Vector3(max.x, lily, min.z);
-			var topRight = new Vector3(max.x, lily, max.z);
-			var bottomLeft = new Vector3(min.x, lily, min.z);
+			var topLeft = new Vector3(min.x, pos.y + lily, max.z);
+			var bottomRight = new Vector3(max.x, pos.y + lily, min.z);
+			var topRight = new Vector3(max.x, pos.y + lily, max.z);
+			var bottomLeft = new Vector3(min.x, pos.y + lily, min.z);
 			Gizmos.DrawLine(bottomLeft, topRight);
 			Gizmos.DrawLine(topLeft, bottomRight);
 			Gizmos.color = oldColor;
