2a91813f6f60cc0c4e98fdb93883b3d229bfb2d1
Tony Le <letony@icloud.com>
2019-11-10 23:07:00 +1100

    Add UltraStateMachine to simply menu flow; Credits menu working; Quit button working; Add MenuButton mb to hold and play button sounds; Convert to using Timeline to handle intro cutscene; Add subtitle system; Add skip button; Load next scene on end of cutscene; General cleanups and renames;

---
+++ Assets/Scripts/UI/Utils/AppQuitter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace StormRend.MainMenus
+{
+	public class AppQuitter : MonoBehaviour
+	{
+		/// <summary>
+		/// Quit the application [CALLBACK]
+		/// </summary>
+		public void Quit()
+		{
+			Debug.Log("Quitting");
+			Application.Quit();
+		}
+   	}
+}
