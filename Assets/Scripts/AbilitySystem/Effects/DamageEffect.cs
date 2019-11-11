f06432ea197b1bc5558853befbdffa57e025202c
Tony Le <letony@icloud.com>
2019-11-11 20:24:31 +1100

    Damage effect can now gain glory on hit or kill

--- Assets/Scripts/AbilitySystem/Effects/DamageEffect.cs
+++ Assets/Scripts/AbilitySystem/Effects/DamageEffect.cs
@@ -1,19 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using pokoro.BhaVE.Core.Variables;
 using StormRend.MapSystems.Tiles;
 using StormRend.Units;
 using StormRend.Utility;
+using StormRend.Utility.Attributes;
 using UnityEngine;
 
 namespace StormRend.Abilities.Effects
 {
 	public class DamageEffect : Effect
 	{
+		//Enums
+		[Flags]
+		public enum GainGloryType
+		{
+			None = 1 << 0,
+			Hit = 1 << 1,
+			Kill = 1 << 2,
+			HitAndKill = 1 << 3,
+		}
+
 		[SerializeField] int damage = 1;
 
-		[Tooltip("Deals multiple damage inline and ONLY at right angles")]
+		[Tooltip("Deals multiple damage inline. DO NOT use on diagonals! Will freeze Unity (infinity loop)")]
 		[SerializeField] bool piercing = false;
+
+		[Space(5)]
+		[SerializeField] GainGloryType gainGlory = GainGloryType.None;
+		[SerializeField] int gloryAmount = 1;
+		[SerializeField] BhaveInt glory = null;
+
+		public override void Prepare(Ability ability, Unit owner)
+		{
+			Debug.Assert(glory, "No glory SOV allocated!");
+		}
 
 		public override void Perform(Ability ability, Unit owner, Tile[] targetTiles)
 		{
@@ -28,30 +50,18 @@ namespace StormRend.Abilities.Effects
 			//Get and convert to lists where required
 			Unit[] units = UnitRegistry.current.aliveUnits;
 
-			if (!piercing)
-			{
-				List<Tile> tt = targetTiles.ToList();
-				foreach (var u in units)
-				{
-					if (tt.Contains(u.currentTile))
-					{
-						//Damage units that are standing on target tiles
-						u.TakeDamage(new DamageData(owner, damage));
-					}
-				}
-			}
-			else
+			if (piercing)
 			{
 				//Only get the first target
 				var target = targetTiles[0];
 
 				//Round the coords it to the nearest 1 or zero
-				Vector3 dirVector = (target.transform.position - owner.currentTile.transform.position).normalized;	//Get normalized direction
-				Vector2Int attackDirection = new Vector2(dirVector.x, dirVector.z).ToVector2Int();	//Convert to v2int direction
+				Vector3 dirVector = (target.transform.position - owner.currentTile.transform.position).normalized;  //Get normalized direction
+				Vector2Int attackDirection = new Vector2(dirVector.x, dirVector.z).ToVector2Int();  //Convert to v2int direction
 
 				//Attack all units in that direction
-				var workingTile = owner.currentTile;		//Start at owner's tile
-				while (workingTile.TryGetTile(attackDirection, out Tile t, true))	//Keep getting tile in direction of the attack
+				var workingTile = owner.currentTile;        //Start at owner's tile
+				while (workingTile.TryGetTile(attackDirection, out Tile t, true))   //Keep getting tile in direction of the attack
 				{
 					//Check is in the list of possible targets
 					if (au.possibleTargetTiles.Contains(t))
@@ -62,12 +72,45 @@ namespace StormRend.Abilities.Effects
 							if (t == u.currentTile)
 							{
 								u.TakeDamage(new DamageData(owner, damage));
+
+								HandleGainGlory(u);
 							}
 						}
 					}
-					workingTile = t;	//Try getting from the new tile
+					workingTile = t;    //Try getting from the new tile
 				}
+			}
+			else
+			{
+				List<Tile> tt = targetTiles.ToList();
+				foreach (var u in units)
+				{
+					if (tt.Contains(u.currentTile))
+					{
+						//Damage units that are standing on target tiles
+						u.TakeDamage(new DamageData(owner, damage));
+
+						HandleGainGlory(u);
+					}
+				}
+			}
+		}
+		void HandleGainGlory(Unit u)
+		{
+			//HIT
+			if ((gainGlory & GainGloryType.Hit) == GainGloryType.Hit || 
+				(gainGlory & GainGloryType.HitAndKill) == GainGloryType.HitAndKill)
+			{
+				glory.value += gloryAmount;
+			}
+			//KILL
+			if ((gainGlory & GainGloryType.Kill) == GainGloryType.Kill ||
+				(gainGlory & GainGloryType.HitAndKill) == GainGloryType.HitAndKill
+				&& u.isDead)
+			{
+				glory.value += gloryAmount;
 			}
 		}
 	}
 }
+
