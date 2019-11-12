67ef98e4de516a513f6944161764fc690ca41f6a
Muhammad Ghalib <mg.gabe108@gmail.com>
2019-11-12 14:20:41 +1100

    Started LevelTransitioner

---
+++ Assets/Scripts/LevelTransitioner/Transitioner.cs
@@ -0,0 +1,16 @@
+﻿using StormRend.Units;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transitioner : MonoBehaviour
+{
+    [SerializeField] List<AllyUnit> allyUnits = new List<AllyUnit>();
+    [SerializeField] List<EnemyUnit> enemyUnits = new List<EnemyUnit>();
+
+    private void Awake()
+    {
+        allyUnits.AddRange(FindObjectsOfType<AllyUnit>());
+        enemyUnits.AddRange(FindObjectsOfType<EnemyUnit>());
+    }
+}
