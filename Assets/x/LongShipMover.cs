82caf77623ee253524b030b15d1c01baf9fdf97d
Chris Poermandya <christopher.poermandya@gmail.com>
2019-11-13 22:55:46 +1100

    LongShip stuff

---
+++ Assets/x/LongShipMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongShipMover : MonoBehaviour
+{
+    public float speed;
+    public GameObject target;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0,0,speed * Time.deltaTime);
+    }
+}
