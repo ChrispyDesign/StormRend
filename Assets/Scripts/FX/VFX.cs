909afd8f20ab9dd96b0d28fc0acf2b59de2fb64b
Tony Le <letony@icloud.com>
2019-11-17 07:35:44 +1100

    VFX totalduration now accounts for startDelay

--- Assets/Scripts/FX/VFX.cs
+++ Assets/Scripts/FX/VFX.cs
@@ -5,9 +5,10 @@ namespace StormRend.VisualFX
     [CreateAssetMenu(menuName = "StormRend/VFX", fileName = "VFX")]
     public class VFX : ScriptableObject
     {
-        [Tooltip("The life time of the VFX in seconds. Set to 0 for infinite")]
+		//Inspector
+		[Tooltip("The life time of the VFX in seconds. Set to 0 for infinite")]
 		public bool autoDuration = false;
-        public float lifetime = 5f;
+       	public float lifetime = 5f;
         public GameObject prefab;
 
 		public float totalDuration		//Of the attached particle
@@ -16,13 +17,19 @@ namespace StormRend.VisualFX
 			{
 				//This might not work
 				var ps = prefab.GetComponentInChildren<ParticleSystem>();
-				return ps.main.duration + ps.main.startLifetime.constant;
+				return ps.main.startLifetime.constant + ps.main.startDelay.constant + ps.main.duration;
 			}
 		}
 
 		public void Create(Vector3 position, Quaternion rotation)
 		{
+			// Debug.Log("Total duration: " + totalDuration);
 			Destroy(Instantiate(prefab, position, rotation), totalDuration);
+		}
+
+		public void Create(Vector3 position, Quaternion rotation, float duration)
+		{
+			Destroy(Instantiate(prefab, position, rotation), duration);
 		}
     }
 }
