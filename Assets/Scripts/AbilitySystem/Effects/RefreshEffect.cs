99470719c71f9c7928d10d0ccc4b25929c35a0ca
Tony Le <letony@icloud.com>
2019-11-20 19:02:48 +1100

    Instant provoke

--- Assets/Scripts/AbilitySystem/Effects/RefreshEffect.cs
+++ Assets/Scripts/AbilitySystem/Effects/RefreshEffect.cs
@@ -15,8 +15,8 @@ namespace StormRend.Abilities.Effects
 			ActAgain = 1 << 1,
 		}
 
-		[Tooltip("The duration before the unit gets auto reselected")]
-		[SerializeField] float reselectTiming = 1f;
+		[Tooltip("The duration before the unit gets re-selected")]
+		[SerializeField] float delay = 1f;
 
 		[Header("NOTE: Refresh must be after Damage Effect")]
 		[ReadOnlyField, SerializeField] int refreshCount = 0;		//internal refresh count
@@ -37,11 +37,11 @@ namespace StormRend.Abilities.Effects
 			
 			//MoveAgain
 			if ((refreshType & RefreshType.MoveAgain) == RefreshType.MoveAgain)
-				(owner as AnimateUnit).SetCanMove(true, reselectTiming);     //You should always be able to move again right?
+				(owner as AnimateUnit).SetCanMove(true, delay);     //You should always be able to move again right?
 
 			//ActAgain
 			if ((refreshType & RefreshType.ActAgain) == RefreshType.ActAgain)
-				(owner as AnimateUnit).SetCanAct(true, reselectTiming);     //You should always be able to move again right?
+				(owner as AnimateUnit).SetCanAct(true, delay);     //You should always be able to move again right?
 
 			//Inc count
 			refreshCount++;
