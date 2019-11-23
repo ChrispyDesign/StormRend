9a72742d8b63c428636faa737dd34357efac33d2
Tony Le <letony@icloud.com>
2019-11-24 03:16:29 +1100

    Simple script to set starting health of a unit; Set to frost troll on lv1

---
+++ Assets/Scripts/Assists/StartingHealth.cs
@@ -0,0 +1,24 @@
+﻿using StormRend.Units;
+using UnityEngine;
+
+namespace StormRend.Assists 
+{ 
+	/// <summary>
+	/// Class to automatically set a unit to a certain starting health
+	/// </summary>
+	public class StartingHealth : MonoBehaviour
+	{
+		[SerializeField] int startingHealth = 3;
+		Unit unit = null;
+
+		void Awake() => unit = GetComponent<Unit>();
+
+		void Start()
+		{
+			Debug.Assert(unit, "No unit found! Disabling..");
+			if (!unit) enabled = false;
+
+			unit.HP = startingHealth;
+		}
+   	}
+}
