7d5d81262117941720a4fde8a71fb6184f54362d
Chris Poermandya <christopher.poermandya@gmail.com>
2019-11-29 11:55:39 +1100

    Added a script to mute the music

---
+++ Assets/Scripts/Audio/MuteMusic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StormRend.Audio
+{
+    public class MuteMusic : MonoBehaviour
+    {
+        public KeyCode mute = KeyCode.M;
+        public AudioSource music;
+        void Start()
+        {
+            music = gameObject.GetComponent(typeof(AudioSource)) as AudioSource;
+        }
+        void ToggleMusic()
+        {
+            // toggle this off/on
+            if (Input.GetKeyUp(mute))
+                // if (gameObject.active == true)
+                //     gameObject.SetActive(false);
+                // else
+                //     gameObject.SetActive(true);
+                // gameObject.SetActive(!gameObject.activeInHierarchy);
+                music.enabled = !music.enabled;
+        }
+        void Update()
+        {
+            ToggleMusic();
+        }
+    }
+}
