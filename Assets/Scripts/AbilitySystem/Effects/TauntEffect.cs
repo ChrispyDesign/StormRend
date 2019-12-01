0ef1a134addaeef9b157fdcbdd03b25af1a99b1b
Tony Le <letony@icloud.com>
2019-12-02 02:23:11 +1100

    Statue level "Blizzard" bug possibly fixed
    Taunt.OnTakeDamage() was not guarding against null damage vendors ie. the blizzard

--- Assets/Scripts/AbilitySystem/Effects/TauntEffect.cs
+++ Assets/Scripts/AbilitySystem/Effects/TauntEffect.cs
@@ -32,6 +32,12 @@ namespace StormRend.Abilities.Effects
 
 		public override bool OnTakeDamage(Unit affectedUnit, HealthData damageData)
 		{
+			//NOTE: THIS IS WHAT WAS CAUSING THE BLIZZARD ISSUES ON THE FINAL LEVEL!
+			//The player was provoking with the berserker when the blizzard would turn over. 
+			//The blizzard passes null for HealthData.vendor, hence why it freezes the game
+
+			if (damageData.vendor == null) return false;	//Exit if it's the blizzard or other null damage dealer
+
 			//Apply reflex damage; The victim attacks back
 			damageData.vendor.TakeDamage(new HealthData(affectedUnit, reflexDamage));
 
