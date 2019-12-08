78364777ff5e7506a66594c24f2d608525a1fda3
Tony Le <letony@icloud.com>
2019-12-09 10:48:03 +1100

    Add license and copyrights

--- Assets/Scripts/Assists/AutoUnitSelector.cs
+++ Assets/Scripts/Assists/AutoUnitSelector.cs
@@ -1,4 +1,12 @@
-﻿using System.Linq;
+/*
+ *  Copyright (C) 2019 Totally Not Birds. All Rights Reserved.
+ *  NOTICE: The intellectual and technical concepts contained herein are proprietary
+ *  to Totally Not Birds and are protected by trade secret or copyright law.
+ *  Dissemination of this information or proproduction of this material is strictly
+ *  forbidden unless prior written permission is obtained forom Totally Not Birds.
+ *  Written by Tony Le <letony@icloud.com> 2019
+ */
+
 using StormRend.States;
 using StormRend.Systems;
 using StormRend.Systems.StateMachines;
--- Assets/Scripts/StateMachine/TurnStates/EnemyTurnState.cs
+++ Assets/Scripts/StateMachine/TurnStates/EnemyTurnState.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections;
+/* 
+ *  Copyright (C) 2019 Totally Not Birds. All Rights Reserved.
+ *  NOTICE: The intellectual and technical concepts contained herein are proprietary
+ *  to Totally Not Birds and are protected by trade secret or copyright law.
+ *  Dissemination of this information or proproduction of this material is strictly
+ *  forbidden unless prior written permission is obtained forom Totally Not Birds.
+ *  Written by Tony Le <letony@icloud.com> 2019
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using pokoro.BhaVE.Core;
 using StormRend.Systems.StateMachines;
